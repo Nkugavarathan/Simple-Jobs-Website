09a6fa45d4009ceba5769035efa46c99aed69365
Nkugavarathan <kugavarathan28@gmail.com>
2025-04-01 12:53:10 +0530

    install tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
-import React from "react"
-
-export default function App() {
-  return <div>App</div>
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold underline text-blue-600">
+        Hello, Tailwind in Vite! 🚀
+      </h1>
+    </div>
+  )
 }
+
+export default App
