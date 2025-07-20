d3cee3bec9750eda8774e3b7649d382e3b6161a5
Nkugavarathan <kugavarathan28@gmail.com>
2025-07-20 10:39:09 +0530

    Router updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
-import Navbar from "./components/Navbar"
-
-import Hero from "./components/Hero"
-import HomeCards from "./components/HomeCards"
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import MainLayout from "./layout/MainLayout"
 import Jobslist from "./components/Jobslist"
+import NotFoundPage from "./pages/NotFoundPage"
 
-import { ViewAllJobs } from "./components/ViewAllJobs"
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/jobs" element={<Jobslist />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    )
+  )
   return (
     <>
-      {/* <Navbar /> */}
-      <Hero />
-      <HomeCards />
-      <Jobslist />
-      <ViewAllJobs />
+      <RouterProvider router={router} />
     </>
   )
 }
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+import Hero from "../components/Hero"
+import HomeCards from "../components/HomeCards"
+import Jobslist from "../components/Jobslist"
+
+import { ViewAllJobs } from "../components/ViewAllJobs"
+function HomePage() {
+  return (
+    <div>
+      <Hero />
+      <HomeCards />
+      <Jobslist />
+      <ViewAllJobs />
+    </div>
+  )
+}
+
+export default HomePage
