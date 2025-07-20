94f25bfd88e8a3621bc1d6f12fb9fe5b1d7254b3
Nkugavarathan <kugavarathan28@gmail.com>
2025-07-20 16:37:04 +0530

    react spinner installed and spinner component updated

--- src/components/Jobslist.jsx
+++ src/components/Jobslist.jsx
@@ -1,11 +1,35 @@
 import React from "react"
 import JobList from "./JobList"
-
+import Spinner from "./Spinner"
+import { useState, useEffect } from "react"
 // import jobData from "../jobs.json"
-import { jobs } from "../jobs.json"
+// import { jobs } from "../jobs.json"
 export default function Jobslist({ isHome = false }) {
   // const recentjobs = jobData.jobs.slice(0, 3) // destr panni edukaddi ippadi eluthanum
-  const fulljobs = isHome ? jobs.slice(0, 3) : jobs
+  // const fulljobs = isHome ? jobs.slice(0, 3) : jobs
+  const [jobs, setJobs] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  const apiUrl = isHome
+    ? "http://localhost:8000/jobs?_limit=3"
+    : "http://localhost:8000/jobs"
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const res = await fetch(apiUrl)
+        const data = await res.json()
+        setJobs(data)
+      } catch (error) {
+        console.log("Error occur while fetch data ...")
+      } finally {
+        setLoading(false) // ✅ Fix typo here!
+      }
+    }
+
+    fetchJobs()
+  }, [])
+
   return (
     <>
       <section className="bg-blue-50 px-4 py-10">
@@ -13,11 +37,16 @@ export default function Jobslist({ isHome = false }) {
           <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
             {isHome ? "Recent Jobs" : "Browse Jobs"}
           </h2>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {fulljobs.map((job) => (
-              <JobList key={job.id} job={job} />
-            ))}
-          </div>
+          {/* loading section */}
+          {loading ? (
+            <Spinner loading={loading} />
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {jobs.map((job) => (
+                <JobList key={job.id} job={job} />
+              ))}
+            </div>
+          )}
         </div>
       </section>
     </>
